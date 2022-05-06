b4a4fc454c83e45a08e74d2ed3d7af501c79e816
Doron <doronking6400@gmail.com>
2022-05-06 17:31:56 +0300

    added react-leaflet component

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import './styles/style.css';
+import '../node_modules/leaflet/dist/leaflet.css';
 import HeaderAndInput from './components/header-and-input';
 import Info from './components/info';
+import Map from './components/map';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
@@ -13,6 +15,7 @@ function App() {
         <HeaderAndInput />
         <Info />
       </div>
+      <Map />
     </>
   );
 }
